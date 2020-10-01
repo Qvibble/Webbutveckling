cfdba3f7402307336daf71be24336d92263dad70
Qvibble <jesper.jensen3131@gmail.com>
2020-10-01 08:54:33 +0200

    4pages klar

--- 4 Pages/2/script.js
+++ 4 Pages/2/script.js
@@ -1,4 +1,5 @@
 let pages = ["Start", "Sida 2", "Sida 3", "Sida 4"];
+let artElements = 10;
 let ul;
 
 window.onload = init;
@@ -11,8 +12,8 @@ function init(){
         createNavElement(pages[i], i);
     }
 
-    for(let i = 0; i < pages.length*10; i++){
-        createArticleElement(pages[i], i);
+    for(let i = 0; i < artElements; i++){
+        createArticleElement(i);
     }
 }
 
@@ -27,8 +28,19 @@ function createNavElement(name, index){
     document.getElementById("ulLi" + index).appendChild(a);
 }
 
-function createArticleElement(name, index){
+function createArticleElement(index){
     let sec = document.createElement("section");
     sec.setAttribute("id", "sec" + index);
     article.appendChild(sec);
+
+    let img = document.createElement("img");
+    img.setAttribute("id", "img" + index);
+    img.setAttribute("src", "../images/simba.gif");
+    sec.appendChild(img);
+
+    let p = document.createElement("p");
+    p.setAttribute("id", "p" + index);
+    p.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    sec.appendChild(p);
+
 }

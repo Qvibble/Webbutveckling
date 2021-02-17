4a994b6ab4a7fd026d1a6913720946c88893d57c
Qvibble <jesper.jensen3131@gmail.com>
2021-02-17 13:42:02 +0100

    index klar

--- Recept/recipe/src/components/parts/HomeMain.jsx
+++ Recept/recipe/src/components/parts/HomeMain.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import {Link, Router, withRouter} from "react-router-dom";
 
 function HomeMain(params) {
     return(
         <main>
             <article>
-                    <h3>Senaste Recept</h3>
-                <section>
-                </section>
-
+                <h3>Senaste Recept</h3>
+                <section></section>
+                <section></section>
+                <section></section>
+                <section></section>
             </article>
             <article>
-                    <h3>Populära Recept</h3>
-                <section>
-                </section>
+                <h3>Populära Recept</h3>                
+                <section></section>
+                <section></section>
+                <section></section>
+                <section></section>
 
             </article>
+            <form>
+                <Link to="search">
+                    <input type="submit" value="Se alla recept"/>
+                </Link>
+            </form>
         </main>
     );
 }
 
-export default HomeMain;
+export default withRouter(HomeMain);

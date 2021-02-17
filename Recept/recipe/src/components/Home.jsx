ff154eacf7844e7005c08e61032ad10e30d98888
Qvibble <jesper.jensen3131@gmail.com>
2021-02-17 12:54:45 +0100

    homemain

--- Recept/recipe/src/components/Home.jsx
+++ Recept/recipe/src/components/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import {Link, Router, withRouter} from "react-router-dom";
 
 import HomeHeader from "./parts/FullHeader";
+import HomeMain from "./parts/HomeMain";
 import Footer from "./parts/Footer";
 
 function Home(props) {
     return(        
         <>
             <HomeHeader/>
-            <main></main>
+            <HomeMain/>
             <Footer/>
         </>
     );
---
+++ Recept/recipe/src/components/parts/HomeMain.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function HomeMain(params) {
+    return(
+        <main>
+            <article>
+                    <h3>Senaste Recept</h3>
+                <section>
+                </section>
+
+            </article>
+            <article>
+                    <h3>Populära Recept</h3>
+                <section>
+                </section>
+
+            </article>
+        </main>
+    );
+}
+
+export default HomeMain;
